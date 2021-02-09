d638e7c697b5fe5426a5355520cf2ffc41638cfa
zeydpxl <Zeyd.Uysal@student.pxl.be>
2021-02-09 22:28:08 +0100

    finished move10 exercise

--- move10.py
+++ move10.py
@@ -1,10 +1,8 @@
 import re
-#TODO: fix error in re.sub
 def move_ten(st):
     woord = ""
-    for item in st:
+    for item in st.lower():
         nummer = ord(item)+ 10
         woord += re.sub(r"([^a-z])", chr(nummer-26), chr(nummer), flags=re.IGNORECASE)
     return woord
-
-print(move_ten("l"))
+print(move_ten("test"))
