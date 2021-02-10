7c0909be6abc72d551973f1a58acb65df8b8a164
zeydpxl <Zeyd.Uysal@student.pxl.be>
2021-02-10 19:09:31 +0100

    finished rot13 code

--- Rot13.py
+++ Rot13.py
@@ -1,3 +1,7 @@
 def rot13(message):
-    return  "".join([(chr(ord(x)+ 13) if (ord(x.lower())+ 13) > ord("z") else chr(ord(x) -13)) for x in message])
-print(rot13("appel"))
+    return "".join(
+        [((chr(ord(x) + 13) if (ord(x.lower()) + 13) <= ord("z") else chr(ord(x) - 13)) if x.isalpha() else x) for x in
+         message])
+
+
+print(rot13("test"))
