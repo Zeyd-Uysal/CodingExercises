4906bdd9e6e774f5752e6c9c4c5955e6539750d6
zeydpxl <Zeyd.Uysal@student.pxl.be>
2021-06-23 04:14:37 +0200

    added more finished exercises

---
+++ JumpingClouds.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'jumpingOnClouds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY c as parameter.
+#
+
+def jumpingOnClouds(c):
+    # Write your code here
+    distance = 0
+    counter = 0
+    while distance -2 >= len(c):
+        counter += 1
+        if c[distance+2] == 1:
+            distance +=1
+        else:
+            distance+=2
+    return counter
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
